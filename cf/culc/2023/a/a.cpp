59e0fb90af0a74e81090c7d632fc3ade4cdd1500
David Santamaria <davidsantamaria1245@gmail.com>
2025-05-27 21:18:23 -0400

    tmp

--- cf/culc/2023/a/a.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int n, k; cin >> n >> k;
-    vector<int> v(n); for (auto &i : v) cin >> i;
-    sort(v.begin(), v.end());
-    cout << accumulate(v.end() - k, v.end(), 0LL) + accumulate(v.begin(), v.end(), 0LL)  << endl;
-    return 0;
-}
--- cf/gym/468004/a.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int t; cin >> t;
-    while (t--) {
-        int n; cin >> n;
-    }
-    return 0;
-}
